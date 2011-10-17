28978df2991ef398cb247d91bdd86d22e8aa1d35
masazzz <masashi.imano@gmail.com>
2011-10-17 09:13:46 +0000

    Put  in quotes in order to suppress replacing meta character like '*'

--- doc/UserGuideJa/Index/toIndexOnly.sh
+++ doc/UserGuideJa/Index/toIndexOnly.sh
@@ -4,7 +4,7 @@ IndexMode="false"
 do
     if [[ "$IndexMode" = "true" ||  "$line" = "Index" ]];then
 	IndexMode="true"
-	echo $line
+	echo "$line"
     fi
 done
 ) | sed -e /^U-[0-9]*/d -e "/^Index U-[0-9]*/d"  -e /^Open∇FOAM-/d -e "s/, [UP]-[0-9]*.*$//"
